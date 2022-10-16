1ce6bfbffc978abfbd43c590b63ef11fadb2f36d
ArtiRam <87929969+ArtiRam@users.noreply.github.com>
2022-10-16 20:34:24 +0300

    Create task_1.py

---
+++ labor_3/task_1.py
@@ -0,0 +1,10 @@
+src = not False and True or False and not True
+
+# TODO расписать упрощение выражения
+# 1) result = True and True or False and False
+# 2) result = True or False
+# 3) result = True
+
+result = True  # TODO подставить результат выражения
+
+print(src == result)
